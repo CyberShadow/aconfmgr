9a36337cbdf494a16aea352871ad1accdeadc1b7
Vladimir Panteleev <git@thecybershadow.net>
2016-09-01 00:13:44 +0000

    Refactor - move collection code into common.sh

--- src/common.sh
+++ src/common.sh
@@ -8,7 +8,7 @@ output_dir=output
 system_dir=system # Current system configuration, to be compared against the output directory
 tmp_dir=tmp
 
-config_save_target=$config_dir/99-unsorted.sh
+warn_size_threshold=$((10*1024*1024))
 
 ignore_paths=(
     '/dev'
@@ -29,30 +29,118 @@ ignore_paths=(
 
 mkdir -p "$config_dir"
 
-function AconfCompile() {
+function AconfAddFile() {
+	mkdir --parents "$(dirname "$system_dir"/files/"$1")"
+	if sudo test -h "$1"
+	then
+		ln -s "$(sudo readlink "$1")" "$system_dir"/files/"$1"
+	else
+		local size
+		size=$(sudo stat "$1" --format=%s)
+		if [[ $size -gt $warn_size_threshold ]]
+		then
+			printf "Warning: copying large file (%s bytes). Add to ignore_paths to ignore.\n" "$size"
+		fi
+		( sudo cat "$1" ) > "$system_dir"/files/"$1"
+	fi
 
-	# Configuration
+	{
+		printf "mode	%s	%q\n" "$(sudo stat --format=%a "$1")" "$1"
+		printf "owner	%s	%q\n" "$(sudo stat --format=%U "$1")" "$1"
+		printf "group	%s	%q\n" "$(sudo stat --format=%G "$1")" "$1"
+	} >> "$system_dir"/file-props.txt
+}
 
+# Run user configuration scripts, to collect desired state into #output_dir
+function AconfCompileOutput() {
 	rm -rf "$output_dir"
 	mkdir "$output_dir"
 	touch "$output_dir"/packages.txt
 	touch "$output_dir"/foreign-packages.txt
 	touch "$output_dir"/file-props.txt
 
+	# Configuration
+
+	typeset -Ag ignore_packages
+	typeset -Ag ignore_foreign_packages
+
 	for file in "$config_dir"/*.sh
 	do
 		printf "Sourcing %s...\n" "$file"
 		source "$file"
 	done
+}
 
-	# System
-
+# Collect system state into $system_dir
+function AconfCompileSystem() {
 	rm -rf "$system_dir"
 	mkdir "$system_dir"
+
+	### Packages
 
 	echo "Querying package list..."
 	pacman --query --quiet --explicit --native  | sort > "$system_dir"/packages.txt
 	pacman --query --quiet --explicit --foreign | sort > "$system_dir"/foreign-packages.txt
+
+	### Files
+
+	# Untracked files
+
+	local ignore_args=()
+	local ignore_path
+	for ignore_path in "${ignore_paths[@]}"
+	do
+		ignore_args+=(-wholename "$ignore_path" -prune -o)
+	done
+
+	echo "Searching for untracked files..."
+
+	local line
+	while read -r -d $'\0' line
+	do
+		#echo "ignore_paths+='$line' # "
+		printf "Found untracked file: %s\n" "$line"
+		AconfAddFile "$line"
+	done < <(																				\
+		comm -13 --zero-terminated															\
+			 <(pacman --query --list --quiet | sed '/\/$/d' | sort --unique | tr '\n' '\0')	\
+			 <(sudo find / -not \(															\
+					"${ignore_args[@]}"														\
+					-type d																	\
+					\) -print0 |															\
+					  sort --unique --zero-terminated) )
+
+	# Modified files
+
+	local ANSI_clear_line="[0K"
+
+	echo "Searching for modified files..."
+	while read -r line
+	do
+		if [[ $line =~ ^(.*):\ \'(.*)\'\ md5sum\ mismatch ]]
+		then
+			printf "%s: %s\n" "${BASH_REMATCH[1]}" "${BASH_REMATCH[2]}"
+			# TODO: Check ignores
+			AconfAddFile "${BASH_REMATCH[2]}"
+		elif [[ $line =~ ^(.*):\  ]]
+		then
+			printf "%s%s\r" "${ANSI_clear_line}" "${BASH_REMATCH[1]}"
+			#echo "Now at ${BASH_REMATCH[1]}"
+		fi
+	done < <(sudo sh -c "stdbuf -o0 paccheck --md5sum --files --backup --noupgrade 2>&1")
+	printf "\n"
+}
+
+# Prepare configuration and system state
+function AconfCompile() {
+
+	# Configuration
+
+	AconfCompileOutput
+
+	# System
+
+	AconfCompileSystem
 
 	# Vars
 
