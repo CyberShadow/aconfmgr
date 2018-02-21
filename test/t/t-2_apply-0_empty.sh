d11b7497f5a4d430df4f04a916420eadfabb706e
Vladimir Panteleev <git@thecybershadow.net>
2018-02-21 19:59:55 +0000

    test: Add basic 'aconfmgr apply' test

---
+++ test/t/t-2_apply-0_empty.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+source ./lib.bash
+
+# Test basic 'aconfmgr apply' functionality.
+
+TestPhase_Run #################################################################
+AconfApply
+
+TestDone ######################################################################
