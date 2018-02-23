f5def8c2ccd015a7ae4408195cdfe563dc1b8767
Vladimir Panteleev <git@thecybershadow.net>
2018-02-23 23:14:35 +0000

    test: Add test for pinning a package

---
+++ test/t/t-2_apply-1_packages-3_unpinned.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+source ./lib.bash
+
+# Test pinning a package.
+
+TestPhase_Setup ###############################################################
+prompt_mode=never
+TestAddPackage test-package native dependency
+TestAddConfig AddPackage test-package
+
+TestPhase_Run #################################################################
+AconfApply
+
+TestPhase_Check ###############################################################
+diff -u "$test_data_dir"/pacman-actions.txt /dev/stdin <<EOF
+pin test-package
+EOF
+
+TestDone ######################################################################
