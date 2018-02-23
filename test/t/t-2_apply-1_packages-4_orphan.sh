3c452eced76cbaf0b7b002d9ea7ffff188d8b9b9
Vladimir Panteleev <git@thecybershadow.net>
2018-02-23 23:19:04 +0000

    test: Add test for removing an orphan package

---
+++ test/t/t-2_apply-1_packages-4_orphan.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+source ./lib.bash
+
+# Test removing an orphan package.
+
+TestPhase_Setup ###############################################################
+prompt_mode=never
+TestAddPackage test-explicit-package native explicit
+TestAddPackage test-dependency-package native dependency
+TestAddPackage test-orphan-package native orphan
+TestAddConfig AddPackage test-explicit-package
+
+TestPhase_Run #################################################################
+AconfApply
+
+TestPhase_Check ###############################################################
+diff -u "$test_data_dir"/pacman-actions.txt /dev/stdin <<EOF
+remove test-orphan-package
+EOF
+
+TestDone ######################################################################
