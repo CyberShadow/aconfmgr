3695bb6366cac13fa7b60d8c73cebba54765dcdc
Vladimir Panteleev <git@thecybershadow.net>
2018-02-24 23:46:23 +0000

    test: Add save test for ignored packages

---
+++ test/t/t-1_save-1_packages-3_ignore.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+source ./lib.bash
+
+# Test for ignored packages.
+
+TestPhase_Setup ###############################################################
+TestAddPackage foo native explicit
+TestAddConfig IgnorePackage foo
+TestAddConfig IgnorePackage bar
+
+TestPhase_Run #################################################################
+AconfSave
+
+TestPhase_Check ###############################################################
+TestExpectConfig < /dev/null
+
+TestDone ######################################################################
