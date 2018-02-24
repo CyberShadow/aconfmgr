82a11b72df1d45b9f968fca478a5a0f0ca2e6b26
Vladimir Panteleev <git@thecybershadow.net>
2018-02-24 23:25:28 +0000

    test: Add test for the GetPackageOriginalFile helper

---
+++ test/t/t-4_helpers-2_getpackageoriginalfile.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+source ./lib.bash
+
+# Test GetPackageOriginalFile helper.
+
+TestPhase_Setup ###############################################################
+TestAddFile /testfile.txt baz
+TestAddPackageFile test-package /testfile.txt foo
+TestCreatePackageFile test-package
+
+TestAddConfig 'echo bar >> $(GetPackageOriginalFile test-package /testfile.txt)'
+
+TestPhase_Run #################################################################
+AconfApply
+
+TestPhase_Check ###############################################################
+diff -u "$test_data_dir"/files/testfile.txt /dev/stdin <<<foobar
+
+TestDone ######################################################################
