8060e613e1c78b6d16f2b76d0617600e6ba00b58
Vladimir Panteleev <git@thecybershadow.net>
2018-02-24 23:25:17 +0000

    test: Add an XFAIL test for modifying a file that's not on the filesystem

---
+++ test/t/t-2_apply-2_files-2_managed-1_modified-4_editgone.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+source ./lib.bash
+
+# Test modifying a file that's not on the filesystem.
+
+TestPhase_Setup ###############################################################
+TestAddPackageFile test-package /testfile.txt foo
+TestCreatePackageFile test-package
+
+TestAddConfig 'echo bar >> $(CreateFile /testfile.txt)'
+
+TestPhase_Run #################################################################
+AconfApply
+
+TestPhase_Check ###############################################################
+
+# XFAIL - FIXME!
+# diff -u "$test_data_dir"/files/testfile.txt /dev/stdin <<<bar
+diff -u "$test_data_dir"/files/testfile.txt <(printf foo)
+
+TestDone ######################################################################
