b3e4db219aacc63d5886eba9c4fe36920b680ae8
Vladimir Panteleev <git@thecybershadow.net>
2018-02-24 05:41:43 +0000

    test: Add test for deleting a package file

---
+++ test/t/t-2_apply-2_files-4_package-1_delete.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+source ./lib.bash
+
+# Test deleting a package file.
+
+TestPhase_Setup ###############################################################
+TestAddFile /testfile.txt foo
+TestAddPackageFile test-package /testfile.txt foo
+TestAddConfig SetFileProperty /testfile.txt deleted y
+
+TestPhase_Run #################################################################
+AconfApply
+
+TestPhase_Check ###############################################################
+test ! -e "$test_data_dir"/files/testfile.txt
+
+TestDone ######################################################################
