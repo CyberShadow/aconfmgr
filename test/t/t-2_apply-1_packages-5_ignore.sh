9a6bb2524b83e4a2f10d280041bf7fe698f6fb69
Vladimir Panteleev <git@thecybershadow.net>
2018-02-24 23:33:07 +0000

    test: Add apply test for ignored packages

---
+++ test/t/t-2_apply-1_packages-5_ignore.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+source ./lib.bash
+
+# Test for ignored packages.
+
+TestPhase_Setup ###############################################################
+TestAddPackage foo native explicit
+TestAddConfig IgnorePackage foo
+TestAddConfig IgnorePackage bar
+
+TestPhase_Run #################################################################
+AconfApply
+
+TestPhase_Check ###############################################################
+test ! -e "$test_data_dir"/pacman-actions.txt
+
+TestDone ######################################################################
