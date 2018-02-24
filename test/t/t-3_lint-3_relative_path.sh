c461ddd22ee652907c663704e480125a554ed00a
Vladimir Panteleev <git@thecybershadow.net>
2018-02-24 21:28:35 +0000

    test: Add test for checking relative paths in configuration

---
+++ test/t/t-3_lint-3_relative_path.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+source ./lib.bash
+
+# Test configuration parsing warning due to relative paths.
+
+TestPhase_Setup ###############################################################
+TestWriteFile "$config_dir"/files/src.txt 'Test file contents'
+TestAddConfig CopyFileTo src.txt dst.txt
+
+TestPhase_Run #################################################################
+AconfCompileOutput
+
+TestPhase_Check ###############################################################
+test $config_warnings -eq 2
+
+TestDone ######################################################################
