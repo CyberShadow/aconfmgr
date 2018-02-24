ea9600a636f693999f93df59721ea5f3dbb21169
Vladimir Panteleev <git@thecybershadow.net>
2018-02-24 06:14:30 +0000

    test: Add test for modifying a lost file

---
+++ test/t/t-2_apply-2_files-1_lost-2_modified-1_file.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+source ./lib.bash
+
+# Test modifying a lost file.
+
+TestPhase_Setup ###############################################################
+TestAddFile /testfile.txt foo
+TestAddConfig 'echo bar > $(CreateFile /testfile.txt)'
+
+TestPhase_Run #################################################################
+AconfApply
+
+TestPhase_Check ###############################################################
+diff -u "$test_data_dir"/files/testfile.txt /dev/stdin <<<bar
+
+TestDone ######################################################################
