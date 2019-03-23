20ccd33267aaa508ae6d48652b51e2d0b4dfc2be
Vladimir Panteleev <git@thecybershadow.net>
2019-03-23 17:44:59 +0000

    test/t/t-2_apply-2_files-2_managed-1_new-1_priority: New test

---
+++ test/t/t-2_apply-2_files-2_managed-1_new-1_priority.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+source ./lib.bash
+
+# Test applying modified priority files.
+
+TestPhase_Setup ###############################################################
+priority_files+=(/dir/file.txt)
+TestAddConfig 'echo "Priority file contents" > $(CreateFile /dir/file.txt)'
+
+TestPhase_Run #################################################################
+AconfApply
+
+TestPhase_Check ###############################################################
+diff -u <(cat /dir/file.txt) /dev/stdin <<<'Priority file contents'
+
+TestDone ######################################################################
