2ebab8ef583074ee4785e331c5647e353748ba2a
Vladimir Panteleev <git@thecybershadow.net>
2019-04-03 21:43:47 +0000

    test/t/t-2_apply-1_packages-6_aur-1_makepkg-7_helperfirst: New test

---
+++ test/t/t-2_apply-1_packages-6_aur-1_makepkg-7_helperfirst.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+source ./lib.bash
+
+# Test that any AUR helpers specified in the configuration are built first, then used.
+TestNeedAUR
+TestNeedAURPackage pacaur da18900a6fe888654867748fa976f8ae0ab96334
+# shellcheck disable=SC2016
+TestNeedAURPackage auracle-git e85c64aeaf9103a6808c3bec136b32fd2fa939ef 'source=("${source[@]/%/#commit=4f90e0fb38e21e9037ccbbe4afa34d3b5299cfa2}")'
+
+TestPhase_Setup ###############################################################
+
+TestAddPackageFile a-test-package /testfile.txt 'File contents'
+TestCreatePackage a-test-package foreign
+
+TestAddConfig AddPackage --foreign pacaur
+TestAddConfig AddPackage --foreign a-test-package
+
+TestPhase_Run #################################################################
+AconfApply
+
+TestPhase_Check ###############################################################
+diff -u <(cat /testfile.txt) <(printf 'File contents')
+
+# Check that it was built by pacaur
+test -f ~/.cache/pacaur/a-test-package/a-test-package-1.0-1-x86_64.pkg.tar.xz
+
+TestDone ######################################################################
