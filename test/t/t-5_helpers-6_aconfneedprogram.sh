8aecf44fa0763504a2d3f9f4f16bd04f81c3a3ca
Vladimir Panteleev <git@thecybershadow.net>
2019-04-06 09:58:15 +0000

    test/t/t-5_helpers-6_aconfneedprogram: New test

---
+++ test/t/t-5_helpers-6_aconfneedprogram.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+source ./lib.bash
+
+# Test AconfNeedProgram helper for native packages.
+TestNeedAUR
+
+TestPhase_Setup ###############################################################
+TestAddPackageFile test-native-package /usr/bin/test-native-program $'#!/bin/sh\necho "Test program!"' 755
+TestAddPackageFile test-foreign-package /usr/bin/test-foreign-program $'#!/bin/sh\necho "Test program!"' 755
+TestCreatePackage test-native-package native
+TestCreatePackage test-foreign-package foreign
+
+TestPhase_Run #################################################################
+AconfNeedProgram test-native-program test-native-package n
+AconfNeedProgram test-foreign-program test-foreign-package y
+
+TestPhase_Check ###############################################################
+/usr/bin/test-native-program
+/usr/bin/test-foreign-program
+
+TestDone ######################################################################
