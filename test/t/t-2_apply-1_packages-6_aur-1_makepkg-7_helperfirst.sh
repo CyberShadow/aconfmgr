e1c6dca02e47356ecbb40041493c1df783f0f187
aconfmgr/renovate <renovate@aconfmgr.thecybershadow.net>
2019-05-30 03:39:06 +0000

    Update dependency auracle-git to v20190529070325

--- test/t/t-2_apply-1_packages-6_aur-1_makepkg-7_helperfirst.sh
+++ test/t/t-2_apply-1_packages-6_aur-1_makepkg-7_helperfirst.sh
@@ -5,7 +5,7 @@ source ./lib.bash
 TestNeedAUR
 TestNeedAURPackage pacaur da18900a6fe888654867748fa976f8ae0ab96334
 # shellcheck disable=SC2016
-TestNeedAURPackage auracle-git e85c64aeaf9103a6808c3bec136b32fd2fa939ef 'source=("${source[@]/%/#commit=4f90e0fb38e21e9037ccbbe4afa34d3b5299cfa2}")'
+TestNeedAURPackage auracle-git d235634675e68a2817656233b55a79209d2fa08d 'source=("${source[@]/%/#commit=4f90e0fb38e21e9037ccbbe4afa34d3b5299cfa2}")'
 
 TestPhase_Setup ###############################################################
 
