c66bef261dc339ebe77a75ebb07b06c6e2ef4275
Vladimir Panteleev <git@thecybershadow.net>
2019-08-18 12:13:41 +0000

    test/t/t-2_apply-1_packages-6_aur-*: Update auracle version

--- test/t/t-2_apply-1_packages-6_aur-1_makepkg-7_helperfirst.sh
+++ test/t/t-2_apply-1_packages-6_aur-1_makepkg-7_helperfirst.sh
@@ -5,7 +5,7 @@ source ./lib.bash
 TestNeedAUR
 TestNeedAURPackage pacaur da18900a6fe888654867748fa976f8ae0ab96334
 # shellcheck disable=SC2016
-TestNeedAURPackage auracle-git 0edc474c5acf43635aed4899da1d100fe061d602 'source=("${source[@]/%/#commit=4f90e0fb38e21e9037ccbbe4afa34d3b5299cfa2}")'
+TestNeedAURPackage auracle-git 0edc474c5acf43635aed4899da1d100fe061d602 'source=("${source[@]/%/#commit=181e42cb1a780001c2c6fe6cda2f7f1080b249e5}")'
 
 TestPhase_Setup ###############################################################
 
