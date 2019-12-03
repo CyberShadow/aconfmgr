2a1153891bb3368f30ba128a656c6be90df6fb08
aconfmgr/renovate <renovate@aconfmgr.thecybershadow.net>
2019-12-03 05:40:32 +0000

    Update dependency auracle-git to v20191022060155

--- test/t/t-2_apply-1_packages-6_aur-2_helpers-2_pacaur.sh
+++ test/t/t-2_apply-1_packages-6_aur-2_helpers-2_pacaur.sh
@@ -4,7 +4,7 @@ source ./lib.bash
 TestNeedAUR
 TestNeedAURPackage pacaur da18900a6fe888654867748fa976f8ae0ab96334
 # shellcheck disable=SC2016
-TestNeedAURPackage auracle-git 0edc474c5acf43635aed4899da1d100fe061d602 'source=("${source[@]/%/#commit=181e42cb1a780001c2c6fe6cda2f7f1080b249e5}")'
+TestNeedAURPackage auracle-git 78e0ab5a1d51705e762b1ca5b409b30b82b897c9 'source=("${source[@]/%/#commit=181e42cb1a780001c2c6fe6cda2f7f1080b249e5}")'
 AconfMakePkg pacaur
 TestAddConfig AddPackage --foreign pacaur
 TestAURHelper pacaur "${XDG_CACHE_HOME:-$HOME/.cache}/pacaur" true
