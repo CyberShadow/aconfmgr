ab98499a9f94e76b4058f8152978f7233864ae27
aconfmgr/renovate <renovate@aconfmgr.thecybershadow.net>
2020-02-28 01:49:13 +0000

    Update dependency yay to v20200227195646

--- test/t/t-2_apply-1_packages-6_aur-2_helpers-5_yay.sh
+++ test/t/t-2_apply-1_packages-6_aur-2_helpers-5_yay.sh
@@ -2,7 +2,7 @@
 source ./lib.bash
 
 TestNeedAUR
-TestNeedAURPackage yay 05a3140025672fcbf684f704e6b062dce483003a
+TestNeedAURPackage yay 57b9ff9b60aebacb4e6a58c95e66dbf038e6b458
 AconfMakePkg yay
 TestAddConfig AddPackage --foreign yay
 TestAURHelper yay "${XDG_CACHE_HOME:-$HOME/.cache}/yay" false
