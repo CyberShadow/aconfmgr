cb3391669b44f3f87cb705385b8c044d009eeb6d
aconfmgr/renovate <renovate@aconfmgr.thecybershadow.net>
2020-07-08 11:38:03 +0000

    Update dependency aurman to v20200707213812

--- test/t/t-2_apply-1_packages-6_aur-2_helpers-4_aurman.sh
+++ test/t/t-2_apply-1_packages-6_aur-2_helpers-4_aurman.sh
@@ -2,7 +2,7 @@
 source ./lib.bash
 
 TestNeedAUR
-TestNeedAURPackage aurman 62160314c33f6d60c7f04c3085ea327df95ba5b1
+TestNeedAURPackage aurman 155a4007d89f7c4b8c5e9900bcdcd4478731a77b
 AconfMakePkg aurman
 TestAddConfig AddPackage --foreign aurman
 TestAURHelper aurman "${XDG_CACHE_HOME:-$HOME/.cache}/aurman" false
