67d33d6b5aac9c2a105222d72ff9512b8dcbd2e3
aconfmgr/renovate <renovate@aconfmgr.thecybershadow.net>
2020-09-14 18:41:45 +0000

    Update dependency yaourt to v20200914094110

--- test/t/t-2_apply-1_packages-6_aur-2_helpers-3_yaourt.sh
+++ test/t/t-2_apply-1_packages-6_aur-2_helpers-3_yaourt.sh
@@ -2,7 +2,7 @@
 source ./lib.bash
 
 TestNeedAUR
-TestNeedAURPackage yaourt 67da8ff148bac693f98d8f3a2e2a2031c20e846f
+TestNeedAURPackage yaourt edf3615e311b6065b9eee29c9699ff6ed0f232b9
 TestNeedAURPackage package-query 61b06791c226f05514ac66f6b86961d16ce6b617
 AconfMakePkg yaourt
 TestAddConfig AddPackage --foreign yaourt
