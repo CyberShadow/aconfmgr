b1180a0d00c43952892c2bb535d857a2d8e4ccad
aconfmgr/renovate <renovate@aconfmgr.thecybershadow.net>
2021-05-05 05:18:52 +0000

    Update dependency paru to v20210504063154

--- test/t/t-2_apply-1_packages-6_aur-2_helpers-6_paru.sh
+++ test/t/t-2_apply-1_packages-6_aur-2_helpers-6_paru.sh
@@ -2,7 +2,7 @@
 source ./lib.bash
 
 TestNeedAUR
-TestNeedAURPackage paru 8bfa71813071a9628407507caea525c971cdf741
+TestNeedAURPackage paru 68659a24734580d64c9c5e0899d60431126c53c3
 AconfMakePkg paru
 TestAddConfig AddPackage --foreign paru
 TestAURHelper paru "${XDG_CACHE_HOME:-$HOME/.cache}/paru" false
