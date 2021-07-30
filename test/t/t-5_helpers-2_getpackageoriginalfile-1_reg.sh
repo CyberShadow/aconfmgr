9358d59214c2e0bc9dc7fb9e6f33b3bbaf473f9e
Milo Mirate <mmirate@gmx.com>
2021-07-30 10:31:33 -0400

    Add a test for GetPackageOriginalFile --no-clobber

--- test/t/t-5_helpers-2_getpackageoriginalfile-1_reg.sh
+++ test/t/t-5_helpers-2_getpackageoriginalfile-1_reg.sh
@@ -4,16 +4,23 @@ source ./lib.bash
 # Test GetPackageOriginalFile helper.
 
 TestPhase_Setup ###############################################################
-TestAddFile /testfile.txt baz
-TestAddPackageFile test-package /testfile.txt foo
+TestAddFile /testfile1.txt baz
+TestAddPackageFile test-package /testfile1.txt foo
+TestAddFile /testfile2.txt baz
+TestAddPackageFile test-package /testfile2.txt foo
 TestCreatePackage test-package native
 
-TestAddConfig 'echo bar >> "$(GetPackageOriginalFile test-package /testfile.txt)"'
+TestAddConfig 'echo spam >> "$(GetPackageOriginalFile test-package /testfile1.txt)"'
+TestAddConfig 'echo bar >> "$(GetPackageOriginalFile test-package /testfile1.txt)"'
+
+TestAddConfig 'echo spam > "$(GetPackageOriginalFile --no-clobber test-package /testfile2.txt)"'
+TestAddConfig 'echo eggs >> "$(GetPackageOriginalFile --no-clobber test-package /testfile2.txt)"'
 
 TestPhase_Run #################################################################
 AconfApply
 
 TestPhase_Check ###############################################################
-diff -u "$test_fs_root"/testfile.txt /dev/stdin <<<foobar
+diff -u "$test_fs_root"/testfile1.txt /dev/stdin <<<foobar
+diff -u "$test_fs_root"/testfile2.txt /dev/stdin <<<spameggs
 
 TestDone ######################################################################
