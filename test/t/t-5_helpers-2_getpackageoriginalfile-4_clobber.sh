934ac70c1a46df7760f3848bbe89f729b3dc03d4
Vladimir Panteleev <git@cy.md>
2021-07-31 14:20:22 +0000

    test/t/t-5_helpers-2_getpackageoriginalfile-4_clobber: New test

---
+++ test/t/t-5_helpers-2_getpackageoriginalfile-4_clobber.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+source ./lib.bash
+
+# Test that the GetPackageOriginalFile helper emits a warning when
+# overwriting a file.
+
+TestPhase_Setup ###############################################################
+TestAddFile /testfile.txt baz
+TestAddPackageFile test-package /testfile.txt foo
+TestCreatePackage test-package native
+
+TestAddConfig 'echo spam >> "$(GetPackageOriginalFile test-package /testfile.txt)"'
+TestAddConfig 'echo eggs >> "$(GetPackageOriginalFile test-package /testfile.txt)"'
+test_expected_warnings+=1
+
+TestPhase_Run #################################################################
+AconfApply
+
+TestPhase_Check ###############################################################
+diff -u "$test_fs_root"/testfile.txt /dev/stdin <<<$'fooeggs'
+
+TestDone ######################################################################
