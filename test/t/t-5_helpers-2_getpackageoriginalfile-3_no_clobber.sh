b9ecd7cde0280690f8c0d1debe96f24a74e355a3
Vladimir Panteleev <git@cy.md>
2021-07-31 09:46:31 +0000

    test/t: Move GetPackageOriginalFile --no-clobber test to own test

--- test/t/t-5_helpers-2_getpackageoriginalfile-3_no_clobber.sh
+++ test/t/t-5_helpers-2_getpackageoriginalfile-3_no_clobber.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 source ./lib.bash
 
-# Test GetPackageOriginalFile helper.
+# Test the --no-clobber option for the GetPackageOriginalFile helper.
 
 TestPhase_Setup ###############################################################
 TestAddFile /testfile.txt baz
 TestAddPackageFile test-package /testfile.txt foo
 TestCreatePackage test-package native
 
-TestAddConfig 'echo bar >> "$(GetPackageOriginalFile test-package /testfile.txt)"'
+TestAddConfig 'echo spam > "$(GetPackageOriginalFile --no-clobber test-package /testfile.txt)"'
+TestAddConfig 'echo eggs >> "$(GetPackageOriginalFile --no-clobber test-package /testfile.txt)"'
 
 TestPhase_Run #################################################################
 AconfApply
 
 TestPhase_Check ###############################################################
-diff -u "$test_fs_root"/testfile.txt /dev/stdin <<<foobar
+diff -u "$test_fs_root"/testfile.txt /dev/stdin <<<$'spam\neggs'
 
 TestDone ######################################################################
