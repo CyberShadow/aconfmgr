2dae04cc850bd93fb07d6ee3ff226b130c8437cb
Vladimir Panteleev <git@cy.md>
2021-07-31 15:17:10 +0000

    test/t/t-1_save-2_files-1_stray-2_empty-2_recreate: New test

---
+++ test/t/t-1_save-2_files-1_stray-2_empty-2_recreate.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+source ./lib.bash
+
+# Test that saving correctly emits a RemoveFile before CreateFile, to
+# avoid creating a configuration which will emit a warning when
+# sourced.
+
+TestPhase_Setup ###############################################################
+TestAddFile /strayfile.txt ''
+TestAddConfig 'echo foo > $(CreateFile /strayfile.txt)'
+
+TestPhase_Run #################################################################
+AconfSave
+
+TestPhase_Check ###############################################################
+TestExpectConfig <<EOF
+RemoveFile /strayfile.txt
+CreateFile /strayfile.txt > /dev/null
+EOF
+
+test ! -e "$config_dir"/files/strayfile.txt # should not exist
+
+TestDone ######################################################################
