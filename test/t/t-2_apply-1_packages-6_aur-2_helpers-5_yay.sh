6e0b24ace3b1b0a736d741275bf0fcff141f52f8
aconfmgr/renovate <renovate@aconfmgr.thecybershadow.net>
2021-10-10 00:39:49 +0000

    Update dependency yay to v20211009233234

--- test/t/t-2_apply-1_packages-6_aur-2_helpers-5_yay.sh
+++ test/t/t-2_apply-1_packages-6_aur-2_helpers-5_yay.sh
@@ -4,7 +4,7 @@ source ./lib.bash
 # Test AUR functionality using yay.
 
 TestNeedAUR
-TestNeedAURPackage yay a62850c2735d8199c22546bf772ebfec03883ed3
+TestNeedAURPackage yay 308e23430e7610ed5065d597e84ecb771530a1b4
 AconfMakePkg yay
 TestAddConfig AddPackage --foreign yay
 TestAURHelper yay "${XDG_CACHE_HOME:-$HOME/.cache}/yay" false
