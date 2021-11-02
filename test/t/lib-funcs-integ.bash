a2db9d9f52514becd283e9921b0e0a7aa2471ea2
aconfmgr/renovate <renovate@aconfmgr.thecybershadow.net>
2021-11-02 23:16:17 +0000

    Update dependency auracle-git to v20211102114542

--- test/t/lib-funcs-integ.bash
+++ test/t/lib-funcs-integ.bash
@@ -483,7 +483,7 @@ function TestNeedPacaur() {
 
 function TestNeedAuracle() {
 	# shellcheck disable=SC2016,SC1004
-	TestNeedAURPackage auracle-git 79ad04336e90ce36c37ec4a4d2e98266aaa90be6 "$(cat <<-'EOF'
+	TestNeedAURPackage auracle-git 003348aafc1b2cc8abbeb11e3a74af79d4709a32 "$(cat <<-'EOF'
 		source=("${source[@]/%/#commit=181e42cb1a780001c2c6fe6cda2f7f1080b249e5}")
 
 		prepare() {
