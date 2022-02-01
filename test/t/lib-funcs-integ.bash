9bbd30b1cadbc34ba9615820e3bef445abd374ed
Vladimir Panteleev <git@cy.md>
2022-02-01 19:27:33 +0000

    test/t/lib-funcs-integ: Patch only the first auracle source
    See https://aur.archlinux.org/cgit/aur.git/commit/PKGBUILD?h=auracle-git&id=26b9371960c723a2d62080e786ea6de062aac9ab

--- test/t/lib-funcs-integ.bash
+++ test/t/lib-funcs-integ.bash
@@ -483,7 +483,7 @@ function TestNeedPacaur() {
 function TestNeedAuracle() {
 	# shellcheck disable=SC2016,SC1004
 	TestNeedAURPackage auracle-git 26b9371960c723a2d62080e786ea6de062aac9ab "$(cat <<-'EOF'
-		source=("${source[@]/%/#commit=181e42cb1a780001c2c6fe6cda2f7f1080b249e5}")
+		source[0]="${source[0]/%/#commit=181e42cb1a780001c2c6fe6cda2f7f1080b249e5}"
 
 		prepare() {
 			cd "$_pkgname"
